6934464e595ff6cd4edb67164f41032d87d77642
xmiphone <youssfnsibi12@gmail.com>
2024-09-19 01:25:23 +0100

    Add files via upload

---
+++ Key.js
@@ -0,0 +1,13 @@
+
+loopy() 
+ function loopy() { 
+ var sWord ="gangxereca" 
+ while (sWord != "@gangxereca") {// Pass
+ sWord = prompt("𝘌𝘯𝘵𝘦𝘳 𝘛𝘩𝘦 𝘒𝘦𝘺 :")  
+ } 
+ 
+//confirm("ok");
+
+window.alert("𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 𝐆𝐚𝐧𝐠 𝐗𝐞𝐫𝐞𝐜𝐚 💉 ");
+
+ } 
